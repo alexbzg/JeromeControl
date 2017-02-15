ed18588b9224ed2f7fdb7bd4e8a9b26e837403ab
alexbzg <alexbzg@gmail.com>
2017-02-15 14:29:26 +0300

    -

---
+++ JeromeControl/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace JeromeControl
+{
+    partial class fControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.expertSyncToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.устройстваToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.expertSyncToolStripMenuItem,
+            this.устройстваToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(317, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // expertSyncToolStripMenuItem
+            // 
+            this.expertSyncToolStripMenuItem.Name = "expertSyncToolStripMenuItem";
+            this.expertSyncToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.expertSyncToolStripMenuItem.Text = "ExpertSync";
+            // 
+            // устройстваToolStripMenuItem
+            // 
+            this.устройстваToolStripMenuItem.Name = "устройстваToolStripMenuItem";
+            this.устройстваToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.устройстваToolStripMenuItem.Text = "Устройства";
+            // 
+            // fControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 25);
+            this.Controls.Add(this.menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menu;
+            this.Name = "fControl";
+            this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem expertSyncToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem устройстваToolStripMenuItem;
+    }
+}
+
